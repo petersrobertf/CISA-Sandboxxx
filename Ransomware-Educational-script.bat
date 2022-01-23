eb0f005d10f5dbfc6aa4c6baa577d9fb9e254b3d
Sudo Science <67087619+petersrobertf@users.noreply.github.com>
2022-01-23 15:16:31 -0500

    Created Ransomware-Educational-script.bat file
    For educational purposes only.

---
+++ Ransomware-Educational-script.bat
@@ -0,0 +1,258 @@
+@echo off
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING**********
+echo Ransomware has been detected on your machine!
+echo **********WARNING**********
+echo TechnicallyRight.Net-Information Security Penetration Testing!
+echo Follow the instructions closely to; experience ransomware, without the consequences...
+echo **********WARNING**********
+echo                                                                                                     
+echo                                                                                                     
+echo                           ``...----..``                                                             
+echo                         `.--::::::/::---.`                                                          
+echo                       `---:/:--....-:::----`                        ``````````                      
+echo                      `-.-/:.`        `.::---.                     ````````````.`                    
+echo                     `:-./:`             .::--.                  `..............-.                   
+echo                     .:.-/`               `:/.:.                 .----.....------:                   
+echo                     -:.::                 ./--:                 ::::-------:::://                   
+echo                     ::`::                  /:-:`                -+///:::::////++:                   
+echo                     ::.::                  /:-:`                 /+++++++++++oo/                    
+echo                  .--::...`.```````         /:-/`                  :oooo+//+oo/.                     
+echo                 -////:-``.--------------...::-:.                    -//.``.-.                       
+echo                 :////:-..-----::::::::::::::::://:-          `.-:::-...----...                      
+echo                 :////::---::::::::::::::::::///////`       `-:////:---:///-.``.`                    
+echo                 ://///:--::::::::/ooo+////////////:       -:////:--:/+o+-..```..`                   
+echo                 -/////:--::::::/ymNNs++//////:///::.......::::--:/++/++:-...``...`                  
+echo                 -/////:::::::::oNNNd/:::::::::::::::::::-.--:////:-` :+:-.........                  
+echo                 ./////::::::://oNNNmso+//+//////////////::/++/:`     +s:-........-.                 
+echo                 ./////::::://///ymNNmmy/+//////++/----//://++/:`   `:o+:-.......---`                
+echo                 .//////::////////omNmy+//////////:    `` -//++/:..-////:-......---:.                
+echo                 `://///::////////+mmho+///++/////-        .////////////+--....---//.                
+echo                 `:////:::://////++mdh+++++++/////-         `.::////+ssys-.....--:+/                 
+echo                  :///:::::://///++yyo++++++/////:.             `/+ooo/-`-....--:++.                 
+echo                  :///::::::::///++++oooo+++/////:`              `..`   `-...---/+/                  
+echo                  ://:::--::::://+++ooooo++//////:                      .....--/++`                  
+echo                  -//::------::://+++ooo++//::///-                      -...--:++-                   
+echo                  .:///+++++++++++++oooo++++++//:.                      :-..--++:                    
+echo                       ````````````````......```                        :--.--//-                    
+echo                                                                        .:--.-:/:.                   
+echo                                                                         :---.-/:-.                  
+echo                                                                         .:--.-::--.                 
+echo              ````````````````````````````````````````````````````````````:-----/:--.                
+echo                                                       ```````````````````....--:::--`               
+echo                                                                   ``........---:::::.               
+echo                                                                    `````````````````    
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+pause
+echo DEFINITION:
+echo A.) Ransomware is malware that employs encryption to hold a victim's information at ransom.
+pause
+echo **********WARNING**********
+echo B.) A user or organization's critical data is encrypted so that they cannot access files, databases, or applications.
+pause
+echo **********WARNING**********
+echo C.) A ransom is then demanded to provided access.
+pause
+echo **********WARNING**********
+echo D.) Ransomware is often designed to spread across a network and target database and file servers, and can thus quickly paralyze an entire organization.
+pause
+echo **********WARNING**********
+echo **********WARNING**********
+echo Please enter your name! For educational purposes only.
+set /p name=
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo Thankyou, %name%!
+echo **********WARNING**********
+echo %name% an event was triggered.
+echo **********WARNING**********
+echo %name% an alert was flagged.
+echo **********WARNING**********
+echo %name% smile and say cheeeese!
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo                                `//:::::---....````````                                              
+echo       `````......``````       -osyssssssssssooooooooo+++/////::::::::---.....                       
+echo   `:yhhsoshmmmmddhyssooo++++++sysoooo++++++++++++++//+++++++++++++++++++++++o:`                     
+echo   -mNNdyhhdmmmdddhhhyhyhoooooosooooo++++++++++++++++///////////////////////:/o/..````````           
+echo  `sNNmhyyhmmmdddhhhyyyhsooooooooooooo++++++++++++++++/////////////////////////o+++++++++++//++os+`  
+echo  .NNmdyyhddmmmddhhhyyyyoooooooooooooo+++++++++++++++/+++///:::///////////////://////+oyosssoosydN+` 
+echo `oMNmhyhhmmmmdddhhyyyhoooooooooooooo+++++++++++++oosso/////+++///:///////////////////oososososydmh. 
+echo `mNNdhyyddmmmddhhhyyyyoooooooooooooo++++++++++oyyhhs+++//////+oooo/://///////////////+osososssshmN- 
+echo .MNmdhyhdmmmmddhhyyyhsoooooooooooooo++++++++shmmmho+++++/:::-...:oyo://///////////////sososososydN+ 
+echo :MNmdhhhdmmmdddhhyyyhooooooooooooooo++++++sdNNMNdo++++++++/:-..```/ys:////////////////oososososydmh`
+echo +MNmdyyddmmmdddhhyyyhoooooooooooooo++++++hNMMMMNy++++//ooo+/::-.  `odo://://///////////sssoosssyhmN`
+echo sMNmdhhddmmmdddhhyyyhooooooooooooo++++++yNMMMMMmy++//:+oo++::/::``.:hh-//://///////////sososossyhmN`
+echo sMNmhhhdmmmmdddhhyyyhoooooooooo+++++++++dMMMMMMmy+//:-/oo++++++/...-yy-//:::///////////sosossssyhmN-
+echo +MNmdyhdmmmmmddhhyhyhoooooooooo+++++++++dMMMMMMNho/:--.:+ooooo+:.-.:yo-//:::://////////sosossssyhmN-
+echo :MNmdhhhmmmNmddhhyyyhooooooooo++++++++++hNMMMMMMNy+/---..:://:--:-:so::/::::://////////sosossssyhmN.
+echo .MNmdhhhmmmmmdddhhyyhsoooooooo+++++++++++dMMMMMMMNy+/:-:::--:://:oho::/::::://////////+ossossssyhmN`
+echo `mNNdhhhdmmmmdddhhyhyhoooooooo++++++++++++dMMMMMMMMdo+///////++oso//++/::::///////////oosoyossyydmd`
+echo  +MNmdhhdmmmmmddhhhyhdoooooooo+++++++++++++smNNNNNNmNds++////////++++/////////////////sosososssydms`
+echo  .NNmdhhdmmmmmdddhhyhhhooooooo+++++++++++++++osyyhhhhyhhyysoooo+++///////////////////+osososssshdN: 
+echo  `oMNmhyhdmmmmmddhhhyhhsooooooo++++++++++++++++++++++++++++//////////////////////////sosososssyddh. 
+echo   -mNNdyhdmmmmmdddhhyyhhoooooooo+o+++++++++++++++++++++++///////////////////////////ooyosoyossydm+` 
+echo    /mNmhshmNNNmmmddhhyyyyyyssssso++++++++++++++++++++////////////////////////+sooooooooooooooshdh-  
+echo     ````...-----------Nmmmmmmmdsysooo+++++++++++++++++++++++++++++++++++++++oo:.`````````````````   
+echo                       MNmmmmmmNmhsssssssssssssssoooooooooooooo+++++++++++++o+-                      
+echo                       MNmmmmmmmmNmdddddddddddddddddddddddddddd/...```````````                       
+echo
+echo ***WARNING:*** 
+echo **********WARNING**********
+echo **********WARNING**********
+echo **********WARNING**********
+echo An image capture was taken of you %name%. We've already began cross referencing your images using artificial intelligence.
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+pause
+echo ***WARNING:*** 
+echo %name%, I'm sorry but this computer was compromised and encrypted by Ransomware!
+echo **********WARNING**********
+echo%name% DO NOT:
+echo Close this window or back out of this program, you must move forward, to retrieve the key and get your information back. 
+echo **********WARNING**********
+echo Your information has been compressed and archived to the cloud. Don't try anything stupid like going to the FBI.
+echo **********WARNING**********
+echo You'll trigger a killchain switch that will disable your motherboards onboard thermastat. 
+echo **********WARNING**********
+echo Your computer will overheat and catch on fire destroying all the evidence.
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo The computer name is %username%!
+echo **********WARNING**********
+set user
+echo **********WARNING**********
+ver
+echo **********WARNING**********
+echo **********WARNING**********
+vol
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Users\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+ipconfig
+echo **********WARNING**********
+echo **********WARNING**********
+set app
+echo **********WARNING**********
+cd c:\Users\%USERNAME%\%APPDATA%\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd C:\Windows\System32\config\systemprofile\AppData\Local
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd
+C:\Windows\System32\config\systemprofile\AppData\LocalLow
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd
+C:\Windows\System32\config\systemprofile\AppData\Roaming
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+set ProgramData
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Program Files\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Users\%USERNAME%\Downloads\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Users\%USERNAME%\Documents\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Users\%USERNAME%\Pictures\
+dir
+echo **********WARNING**********
+echo **********WARNING**********
+cd c:\Users\%USERNAME%\Videos\
+dir
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo 
+echo                                                                                                     
+echo                   .:+ooooooooooooooooooooooooooooooooooooooooooooooooooooooooo+/:.                  
+echo                `/yysooo+++++++++++++++++++++++++++ooooooooooooooooooooooooooooosyhs-                
+echo               `syso+++++++++++++++++++++++++++++++++++++ooooooooooooooooooooooooosyh+               
+echo               oyoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooyh:              
+echo               ysooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooosh+              
+echo               hyssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssho              
+echo               hysyyyyssyysssssssyssyyssssssssssysssyssssssssyssssyyssssssssssssssssyho              
+echo               yysssyysooyyooooooysoshsooooooooshooyyoooooooyyoosyyssssssssssssssssssh+              
+echo               yysssyhhyooyyoooooyhoohyooooooooyyoohsoooooshyooyhyssssssssssssssssssyh+              
+echo               yyssssyhhysshyssssyhsoyhyyyyyyyyhssyhyysssshysshhyyyyyyyyyyyyyyyyyyyyyh+              
+echo               yyyyyyyyhdhsshhhhhhhyssyyyyyyyyyysssyyyyyhhysyhhyyyyyyyyyyyyyyyyyyyyyyh+              
+echo               yyyyyyyyyhdhyyyyyyyyyyhhhhhhhhhhhhhhhhhhyyyyydhyyyyyyyyyyyyyyyyyyyyyyyh+              
+echo               yyyyyyyyyyhddhhhdhdhhyhsysssoo::::::///+ooshdhyyyyyyyyyyyyyyyyyyyyyyyyy+              
+echo               yyyyyyyyyyyhysy+y/s:s:o:o:o:o`````.:/++++:`.dyyyyyyyyyyyyyyyyyyyyyyyyyy+              
+echo               yyyyyyyyyyyy/o/o/s/o/o/o/o/o-``.:/:--:--`.`-hyyyyyyyyyyyyyyyyyyyyyyyyyy+              
+echo               yyyyyyyyyyh/o+o/o/+/+/+++++:`````.:+++++:  yyyyyyyyyyyyyyyyhhyyyhyyyyyy+              
+echo               yyyyyyyyyy/o+o/o/++++/+++//`````/soo:oys`  syyyyyyyyyyyyhsddddhdhhhyyyy+              
+echo               yyyyyyyyy/o+o/o/++/+/o/o/+`````    `..:+`  `yyyyyyyyyyyyy+sy+ssydddyyyy+              
+echo               yyyyyyyy/oooo+++s+s+o:s:s``````      -`     `yyyyyyyyyyhs-+o.yy.yyoshyy+              
+echo               yyyyyyh++s+++s/s/++/s/soo+/-``         .-  ```hyyyyyyyyy/oy-oo--+/+yyyy+              
+echo               yyyyyyh+o+/o/s/o/+++s+o//+/o``   `-`   .:-/+ooyyyyyyyyys/o/.++.os/syyyy+              
+echo               yyyyyyyso+o+oos++o+oy/o+/o/o``  `+.: `-/oo.:yyyyyyyyyyy/+o-+s:+y/oyyyyy+              
+echo               yyyysys+o+oos+oo+o+o/soo+/:```  `/////+::+syyyyyyyyyyyo/s+`..`-/+syyyyy+              
+echo               yyo+oo+s++/o+++oo+/+/+/s+.`````  ``````...-hyyyyyyyyyys::+::/.`.oyyyyyy+              
+echo               yh/s/o+s++y:o/+/s/o/o/o:://.````          /yyyyyyyyyo/..`-/oho/+yyyyyyy+              
+echo               yhs+ooo+shdo+++o+s+s+s+++++oo+-.```     `.oyyyyyyys-```/o+shhhhsyyyyyyy+              
+echo               sd+/y:/hddysoooosssssssssssssssooosooooosydysyyyyo.``` ss+s/:/syyyyyyyy+              
+echo               +h-/y`.ddhoooohhhhhoshhhhhy+shhhhho/yhdddhs::shsh:`````++/o.::yyyyyyyyy+              
+echo               :++hy`-dhhs/+yhhhhh/+hhhhh///shhhs//+hhhho::sy++hhs/.```-oo--/yyyyyyyyy+              
+echo               ++:hy`-dhhyssyyyyyyssyyysssosssssssoyhhh+:/yy/ohhhhhys/.`oh/:+yyyyyyyyy+              
+echo               +h:+y`-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy/:+ys/ohhhhhhhhhho+yyyyyyyyyyyyy+              
+echo               oh/:y`-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs::+ho/o:shhhhhhhhhhhhhyyyyyyyyyyy+              
+echo               -y-oy`.hhhhhhhhhhhhhhhhhhhhhhhhhhhhho::oh+/+`/+/+yhhhhhhhhhhhhhyyyyyyyy+              
+echo               +-ohy`.hhhhhhhhhhhhhhhhhhhhhhhhhhhh+:/sy/+/`+/+:oyysyyhhhhhhhhhhhhyyyyy:              
+echo               .y-oy.-hhhhhhhhhhhhhhhhhhhhhhhhhhy/:/yy/o:.os.oo/s/+y/syhhhhhhhhhhhhhyo               
+echo                .+shhhhhhhhhhhhhhhhhhhhhhhhhhhhs/:+hs/o--s/:osso/:/ho+++yoyhhhhhhhhy/                
+echo                  `:+syyyyyyyyyyyyyyyyyyyyyyyyo/:+yo/o.:+:/:+so////y:::/y/+yyyyys+:`                 
+echo                  .:///////////////////////////////////////////////////////+++++//-`                 
+echo       ...`     .://///////////::::::::::::::::::://///::///////////////////////////:` `..`          
+echo       sss:    .:::::::::::+ss+:::::::::::::::::::ssso+::::oosssso/:::::::::::::::::::`:ss/          
+echo       yyy:`-/+oo/::/oooo+:+yy+:+oo:-----/+ooo+::oyyyo+----::yyy+::+oo+oo+::+oooo+:/+o++yy+ -/++/:`  
+echo       yyy:/yy+syy++yyooyyo+yy+oyy+-----:yyy/yyy:+yyy/:------yyy+--syy+syy+oyyosyy+oyy+oyy+/yyo+yy+  
+echo       yyy/+ss-oyy+oyy+/yyo+yyyyyo......:yys-yyy-:yys........yyy/..yyy:oyy+oss/oyy+oyy//yy++yy/:yyo  
+echo   --- hyy/./osyyy+oyy/-//:+yyyyh/......:yys.yyy.:hyy`.......hyy/..yyh:---.-/syyyy+oyy//yyo+yyssyyo  
+echo  `hhh hhh/+hh/ohh+ohh/:ss++hhyhhy-`````:hhs.hhh.:hhy````````hhh/``yhh-````ohh/ohh+ohh//hho+hh+/ss+  
+echo  `hhh.hhh/shh:+hh+ohh+/hho+hh+ohhy.    -hhy.hhh`-hhy       `hhh/ `yhh-    yhh:ohh+ohh//hho+hh+/hhs  
+echo   /syyyyo`/yyssyy/-syyyys-/yy/`syyo    `oyysyy+ -yys        yyy:  syy-    +yyssyy/:syysyy+.syysys-  
+echo     ````   `.`````  `..`  ```` ````      ``.``   ```        ```   ```      `.`````  `.````  `..`    
+echo                       -+-             //////.          .+/////           ./////-                    
+echo                      -y/h-            yo---yo          -d:--:d.          oy:::/:                    
+echo                     -ho/sh-           ys++++.          -d++++/           -+///ss                    
+echo                    `s-...:s.          o/               .y`               +o+++o+     
+echo
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo Company: Jack of Trade Apps
+echo Author: SudoScience (Rob Peters)
+echo **********WARNING**********
+echo WARNING: for EDUCATION ONLY
+echo DISCLAIMER: Absolutely no RATS, Trojan horses, or FoxyProxies were injured while testing this.
+echo **********WARNING**********
+echo **********WARNING**********
+echo Check out https://technicallyright.net for more hacks.
+echo **********WARNING**********
+echo **********WARNING**********
+echo "This is the end, my only friend, the end!" ~ The Doors
+pause
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo "Mischeif managed!"
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+echo **********WARNING********************WARNING********************WARNING********************WARNING********************WARNING***********
+pause
+cls
